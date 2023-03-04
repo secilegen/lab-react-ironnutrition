ba0c1d25762c021e6af015b64f657e874cd64288
Secil <secilegen@gmail.com>
2023-03-04 15:34:20 +0100

    iterations 1-4 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import foods from "./foods.json"
+import FoodBox from './components/FoodBox';
+import {Divider, Row, Button} from 'antd'
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
+
+  const [foodList, setFoodList] = useState(foods)
+
+  const addNewFood = (newFoodItem) => {
+    const updatedFoodList = [ newFoodItem, ...foodList]
+    setFoodList(updatedFoodList)
+    console.log('Added item is:', newFoodItem)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddFoodForm addNewFood={addNewFood}/>
+
+      {/* <Button> Hide Form / Add New Food </Button> */}
+     
+       <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+      {foodList.map((food)=>{
+        return(
+        // <div>
+        //   <p>{food.name}</p>
+        //   <img src={food.image} width={80}/>
+        //   </div>
+        <FoodBox food={food} />
+        )
+      })}
+      </Row>
     </div>
   );
 }
